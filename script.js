eb893d90d8b18ee536779c11056de1a13771f6aa
GorasiyaNemish <nemishgorasiya@gmail.com>
2023-10-25 17:14:18 +0530

    Till Page4

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
+
 const text = document.querySelector("#border-text p");
 
 text.innerHTML = text.innerHTML.split("").map((char,i)=> `<span style="transform: rotate(${i*10}deg)">${char}</span>`).join("");
